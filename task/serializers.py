9ccf9bc8710f59a93df4469392410f81c3d96d9b
olatunde2 <133256292+olatunde2@users.noreply.github.com>
2023-09-14 17:03:11 +0300

    Update serializers.py

--- task/serializers.py
+++ task/serializers.py
@@ -8,7 +8,7 @@ class PersonSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-     def validate_name(self, value):
+    def validate_name(self, value):
         if any(char.isdigit() for char in value):
             raise serializers.ValidationError("Name cannot contain numbers.")
             return value
