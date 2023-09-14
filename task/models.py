4833daaa9e870542d08014dde57c767258126548
olatunde2 <133256292+olatunde2@users.noreply.github.com>
2023-09-14 16:20:49 +0300

    Update models.py

--- task/models.py
+++ task/models.py
@@ -2,7 +2,7 @@ from djongo import models
 
 class Person(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    
+    age = models.IntegerField()
 
     def _str_(self):
         return f"{self.name}"
