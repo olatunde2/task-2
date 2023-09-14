3b81a362853387a5d330bc6f6bb53fb008289b48
olatunde2 <133256292+olatunde2@users.noreply.github.com>
2023-09-14 16:20:09 +0300

    Update views.py

--- task/views.py
+++ task/views.py
@@ -1,35 +1,8 @@
-# views.py
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets
 from .models import Person
 from .serializers import PersonSerializer
-from rest_framework import generics
 
-class PersonListCreateView(generics.ListCreateAPIView):
+
+class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def person_detail(request, pk):
-    try:
-        person = Person.objects.get(pk=pk)
-    except Person.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = PersonSerializer(person)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = PersonSerializer(person, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        person.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-        
-lookup = "id"
