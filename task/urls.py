3407cd06173744524b8454ec018c793849f8ae6f
olatunde2 <133256292+olatunde2@users.noreply.github.com>
2023-09-14 16:19:10 +0300

    Update urls.py

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,11 @@
-# appname/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PersonViewSet
 
-from django.urls import path
-from . import views
+router = DefaultRouter()
+router.register('api', PersonViewSet)
 
 urlpatterns = [
-    path('api/', views.PersonListCreateView.as_view(), name='person-list-create'),
-    path('api/<int:pk>', views.person_detail, name='person-detail'),
+    path('', include(router.urls)),
+
 ]
